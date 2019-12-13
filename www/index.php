1468c2469dc82b28a5f362345f6c2a0f386432ad
m4niak <kuba@imsolution.it>
2019-12-13 09:59:51 +0100

    www/index.php

--- www/index.php
+++ www/index.php
@@ -1,15 +1,16 @@
 <?php
-// phpinfo();
-//// Create connection
-$host_db = "dockerdb";
-$username = "root";
-$password = "root";
-$database = "database";
-$conn = new mysqli($host_db, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+phpinfo();
 ?>
-echo "Connected successfully";
+//// Create connection
+//$host_db = "dockerdb";
+//$username = "root";
+//$password = "root";
+//$database = "database";
+//$conn = new mysqli($host_db, $username, $password, $database);
+// Check connection
+//if ($conn->connect_error) {
+   // die("Connection failed: " . $conn->connect_error);
+//}
+
+//?>
+//echo "Connected successfully";
