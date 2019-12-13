2a731cdbac648802de7538c17befa2c898f9b374
m4niak <kuba@imsolution.it>
2019-12-13 10:22:33 +0100

    index.php change

--- www/index.php
+++ www/index.php
@@ -9,8 +9,8 @@ $database = "database";
 $conn = new mysqli($host_db, $username, $password, $database);
 Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-
 echo "Connected successfully";
+
